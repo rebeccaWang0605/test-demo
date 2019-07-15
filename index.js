0c7696edeb00b86d846aa00c62e55b14d01c42f6
rebeccaWang <13717184357@163.com>
2019-07-15 21:42:28 +0800

    test

--- index.js
+++ index.js
@@ -1,17 +1,2 @@
 console.log('a')
-<<<<<<< HEAD
-console.log('brush teeth')
-console.log('sleep')
-<<<<<<< HEAD
-var a = 10;
-var b = 10;
-console.log(a + b )
-=======
-var a = 20;
-var c = 10;
-console.log(a + c)
->>>>>>> dev
-=======
-console.log('wake up');
-console.log('brush teeth')
->>>>>>> 1e7960654a8e8853b9ae7236f3395d970f850821
+console.log('test')
